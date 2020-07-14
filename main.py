3a2e4a791d56f96b983fda18fe54bce6d7e0e8b7
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-14 17:59:44 +0000

    worked on it.

---
+++ main.py
@@ -0,0 +1,36 @@
+print("Hello World")
+
+name = "Sanaa Montgomery"
+
+print(name)
+
+FavoriteSport = "Basketball"
+
+print(FavoriteSport)
+
+FavoriteMusician = "Jhene Aiko" 
+
+print(FavoriteMusician)
+
+Vacation = "Myrtle Beach Safari"
+
+print(Vacation)
+
+Hobby = "Nothing"
+
+print(Hobby)
+
+TodaysDate = "July 14th 2020" 
+
+print(TodaysDate)
+
+a = "Sanaa"
+b = "Basketball"
+c = "Jhene Aiko"
+d = "Myrtle Beach Safari "
+e = "Nothing"
+f = "July 14th 2020"
+
+print (" My name is " + a + ". I love "+ b + " . and I love " + c + "." )
+
+print ( " A Vacation I want to go on is " + d + ". My hobby is "+ e + ". Todays Date  is  " +  f  + ".")
